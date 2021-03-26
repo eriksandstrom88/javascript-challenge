dfd4ed94a93d8cf746baef2b9b000dca4d62971c
Erik Sandstrom <eriksandstrom88@gmail.com>
2021-03-26 11:38:44 -0600

    10

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,20 +1,26 @@
-// index.js for javascript-challenge
+// app.js for javascript-challenge
+
+// select the bable body using d3
 var bodybodybody=d3.select("tbody");
 
+//populate the table with data
 data.forEach((ufo_sighting)=>{
   var rowrowrow=bodybodybody.append('tr');
   Object.entries(ufo_sighting).forEach(([key, value]) => {
     rowrowrow.append('td').text(value);
 })});
 
-// var dateEntered=d3.select('.datetime');
-// var filterButton=d3.select('.filter-btn');
+
+// copy the data and select the filter using d3
 var copy_data=data
 var filterButton=d3.select('#filter-btn');
 var form=d3.select('form');
+
+// give instructions for what should happen when the button is clicked or user hits enter
 filterButton.on("click",runEnter);
 form.on("submit",runEnter);
 
+// define the function that is executed when the user clicks the button or hits enter
 function runEnter() {
   d3.event.preventDefault();
   bodybodybody.text("");
